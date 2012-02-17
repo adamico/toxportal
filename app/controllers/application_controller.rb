59a05f9daadea7ebcb6973984c48f69c4adb8dae
Andrea D'Amico <andrea.damico@gmail.com>
2012-02-17 11:49:33 +0100

    forcing authentication, adding flash methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate_user!
+
   protect_from_forgery
+
+  def redirect_with_flash(resource, path=nil, message=nil)
+    path = resource unless path
+    resource = resource[1] if resource.is_a?(Array)
+    message = flash_message(resource) unless message
+    redirect_to path, :notice => message
+  end
+
+  private
+
+  def flash_message(resource)
+    translated_resource = t('activerecord.models.' + resource.class.name.downcase).classify
+    @flash_message = t("flash.#{self.action_name}", :resource => translated_resource)
+  end
 end
