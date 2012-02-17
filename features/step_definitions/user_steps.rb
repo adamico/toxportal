9540cb9c624067ffe3b8fc59da77e67bacfdd2af
Andrea D'Amico <andrea.damico@gmail.com>
2012-02-17 14:39:55 +0100

    fr translation for login stuff

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -17,7 +17,7 @@ end
 def sign_in user
   visit '/users/sign_in'
   fill_in "Email", :with => user[:email]
-  fill_in "Password", :with => user[:password]
+  fill_in "Mot de passe", :with => user[:password]
   click_button "Connexion"
 end
 
