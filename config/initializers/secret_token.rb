f95f36fc25294e5ed0a5787d5892b255b16d65b6
Andrea D'Amico <andrea.damico@gmail.com>
2012-02-17 11:03:06 +0100

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toxportal::Application.config.secret_token = 'fd5519cc352dfe8c69dd3752e4641be7436c394cb1bf27756911d330ccd600e23ea85e4f56305fb512d29007615f8bb94a8a7995b16c2e2a7c6b6b3da1cf36bf'
