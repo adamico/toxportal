dd5ec18e85dab051baa9e4eb252d9a8a1235e6d4
Andrea D'Amico <andrea.damico@gmail.com>
2012-03-14 10:53:17 +0100

    custom resource, use slugs

--- app/admin/substances.rb
+++ app/admin/substances.rb
@@ -1,3 +1,21 @@
+# encoding: UTF-8
 ActiveAdmin.register Substance do
-  
+  controller do
+    defaults finder: :find_by_slug
+  end
+  form do |f|
+    f.inputs do
+      f.input :name, as: :string, label: "Libéllé"
+    end
+    f.buttons
+  end
+  index do
+    column :name
+    default_actions
+  end
+  show title: :name do |technique|
+    attributes_table do
+      row :name
+    end
+  end
 end
